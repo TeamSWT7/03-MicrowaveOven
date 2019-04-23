2905bd1323398e0392c039b71c0be70299231699
Mathias7120 <mgade7120@gmail.com>
2019-04-23 13:20:15 +0200

    setup

--- Microwave.Test.Integration/IT1_Light_Display_Powertube.cs
+++ Microwave.Test.Integration/IT1_Light_Display_Powertube.cs
@@ -4,16 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
-    public class Class1
+    public class IT1_Light_Display_Powertube
     {
+        private Light _light;
+        private Display _display;
+        private PowerTube _powerTube;
+        private IOutput _output;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _light = new Light(_output);
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+        }
+
+        #region Light integrationtest
+
         [Test]
         public void Test()
         {
-            Assert.AreEqual(true, true);
+
         }
+
+        #endregion
     }
 }
