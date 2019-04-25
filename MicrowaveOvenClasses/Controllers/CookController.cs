edaf1bd4a540fbe93d34590b026211d3f7c4495c
Mathias7120 <mgade7120@gmail.com>
2019-04-25 17:59:36 +0200

    Fixed Timer Errors

--- MicrowaveOvenClasses/Controllers/CookController.cs
+++ MicrowaveOvenClasses/Controllers/CookController.cs
@@ -64,6 +64,9 @@ namespace MicrowaveOvenClasses.Controllers
         public void OnTimerTick(object sender, EventArgs e)
         {
             int remaining = myTimer.TimeRemaining;
+
+            remaining = remaining / 1000;
+
             myDisplay.ShowTime(remaining/60, remaining % 60);
         }
     }
