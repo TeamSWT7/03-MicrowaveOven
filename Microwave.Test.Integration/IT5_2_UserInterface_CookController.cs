f31cbd1e961422f5d71c9b2aa0ecbf5c5e995d9c
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-30 11:18:26 +0200

    Added file

---
+++ Microwave.Test.Integration/IT5_2_UserInterface_CookController.cs
@@ -0,0 +1,20 @@
+﻿using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT5_2_UserInterface_CookController
+    {
+        // UUT
+        private UserInterface _userInterface;
+        private CookController _cookController;
+
+        // Output
+        private IOutput _output;
+
+        // 
+
+    }
+}
