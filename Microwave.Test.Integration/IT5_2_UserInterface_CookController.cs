c158a8765ef49b0ef184fc832d7f46d950553d72
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-30 11:52:27 +0200

    Added test, doesn't work

--- Microwave.Test.Integration/IT5_2_UserInterface_CookController.cs
+++ Microwave.Test.Integration/IT5_2_UserInterface_CookController.cs
@@ -1,4 +1,7 @@
-﻿using MicrowaveOvenClasses.Controllers;
+﻿using System;
+using System.Runtime.InteropServices;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
@@ -35,20 +38,42 @@ namespace Microwave.Test.Integration
             _buttonTimer = Substitute.For<IButton>();
             _buttonStartCancel = Substitute.For<IButton>();
             _door = Substitute.For<IDoor>();
-            _light = Substitute.For<ILight>();
-
-            // CookController
-            _timer = Substitute.For<ITimer>();
-            _display = Substitute.For<IDisplay>();
-            _powerTube = Substitute.For<IPowerTube>();
-
+            
             #endregion
+
+            _output = Substitute.For<IOutput>();
+
+            _light=new Light(_output);
+
+            _timer = new Timer();
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
 
             _cookController = new CookController(_timer, _display, _powerTube);
             _userInterface = new UserInterface(_buttonPower, _buttonTimer, _buttonStartCancel, _door, _display, _light,
                 _cookController);
         }
 
+        [TestCase(0, 50)]
+        [TestCase(1, 100)]
+        [TestCase(2, 150)]
+        [TestCase(3, 200)]
+        [TestCase(4, 250)]
+        [TestCase(5, 300)]
+        [TestCase(6, 350)]
+        public void Press_SetTimeState_CookingStartsCookingWithCorrectPower(int timesPowerButtonPressed, int power)
+        {
+            _userInterface.OnPowerPressed(null, EventArgs.Empty);
+            for (int i = 0; i < timesPowerButtonPressed; i++)
+            {
+                _userInterface.OnPowerPressed(null, EventArgs.Empty);
+            }
+            _userInterface.OnTimePressed(null, EventArgs.Empty);
 
+            _output.ClearReceivedCalls();
+
+            _userInterface.OnStartCancelPressed(null, EventArgs.Empty);
+            _output.Received(1).OutputLine($"PowerTube works with {power} W");
+        }
     }
 }
