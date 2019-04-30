52339182b24e47ecc88df783d6ada59c4043b989
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-30 11:35:07 +0200

    Added SetUp

--- Microwave.Test.Integration/IT5_2_UserInterface_CookController.cs
+++ Microwave.Test.Integration/IT5_2_UserInterface_CookController.cs
@@ -1,5 +1,6 @@
 ﻿using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
@@ -14,7 +15,39 @@ namespace Microwave.Test.Integration
         // Output
         private IOutput _output;
 
-        // 
+        // Fakes for UserInterface Paramaters
+        private IButton _buttonPower;
+        private IButton _buttonTimer;
+        private IButton _buttonStartCancel;
+        private IDoor _door;
+        private IDisplay _display;
+        private ILight _light;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+
+        [SetUp]
+        public void SetUp()
+        {
+            #region Substitutes for paramaters for UserInterface and CookController
+
+            // UserInterface
+            _buttonPower = Substitute.For<IButton>();
+            _buttonTimer = Substitute.For<IButton>();
+            _buttonStartCancel = Substitute.For<IButton>();
+            _door = Substitute.For<IDoor>();
+            _light = Substitute.For<ILight>();
+
+            // CookController
+            _timer = Substitute.For<ITimer>();
+            _display = Substitute.For<IDisplay>();
+            _powerTube = Substitute.For<IPowerTube>();
+
+            #endregion
+
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_buttonPower, _buttonTimer, _buttonStartCancel, _door, _display, _light,
+                _cookController);
+        }
 
 
     }
